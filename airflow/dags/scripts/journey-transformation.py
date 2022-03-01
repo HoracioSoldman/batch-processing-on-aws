6ff500e2f982acc06e36fe0e258f8d317c0f79d1
Horacio Soldman <horaciosoldman@gmail.com>
2022-03-01 23:13:59 +0300

    Upd: update spark jobs to support dim_datetime

--- airflow/dags/scripts/journey-transformation.py
+++ airflow/dags/scripts/journey-transformation.py
@@ -1,75 +1,143 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# ## Transformation for rental journey data 
+# This notebook is responsible for transforming journey data by performing the following tasks:
+# 
+#     1. Renaming columns (removing spaces and lowercasing)
+# 
+#     2. Convert data types from string to timestamps
+#     
+#     3. Attach weather dates
+#     
+#     4. Drop unnecessary columns
+#     
+#     5. Update extra files for dimension tables
+
 import pyspark
 import os
 
-from pyspark.sql import SparkSession, functions as F, types as T
+from pyspark.sql import SparkSession
 
-
-spark = SparkSession.builder.master('local[*]')\
+spark = SparkSession.builder\
+    .master('local[*]') \
     .appName('journey-and-stations-data-transformer')\
-    .enableHiveSupport()\
     .getOrCreate()
 
-
+# get journey data
 df_journey = spark.read.csv("s3a://hrc-de-data/raw/cycling-journey/*/*", inferSchema=True, header=True)
 
-df_journey= df_journey.withColumnRenamed('Rental Id', 'rental_id')\
-    .withColumnRenamed('Bike Id', 'bike_id')\
-    .withColumnRenamed('Start Date', 'start_date')\
-    .withColumnRenamed('End Date', 'end_date')\
-    .withColumnRenamed('StartStation Id', 'start_station')\
-    .withColumnRenamed('EndStation Id', 'end_station')
+df_journey.take(2)
+
+df_journey.printSchema()
+
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+# rename columns
+df_journey= df_journey.withColumnRenamed('Rental Id', 'rental_id').withColumnRenamed('Bike Id', 'bike_id').withColumnRenamed('Start Date', 'start_date').withColumnRenamed('End Date', 'end_date').withColumnRenamed('StartStation Id', 'start_station').withColumnRenamed('EndStation Id', 'end_station')
+
+# convert data types
+df_journey= df_journey.withColumn('start_date', to_timestamp(col('start_date'), 'dd/MM/yyy HH:mm'))
+
+df_journey= df_journey.withColumn('end_date',  to_timestamp(col('end_date'), 'dd/MM/yyy HH:mm'))
+
+# add weather_date column
+df_journey= df_journey.withColumn('weather_date', to_date(col("start_date"), 'dd/MM/yyy HH:mm'))
 
 
-# drop unnecessary column
-df_journey= df_journey.drop('Duration')
+df_journey.show(5)
+df_journey.printSchema()
 
-# add the weather_id column
-df_journey= df_journey.withColumn('weather_date', F.to_date(df_journey.start_date))
 
-'''
-we want to complete the stations data with some additional stations 
-which are not present in the original stations data but are seen in some journey.
-'''
-df_processed_stations= spark.read.parquet('s3a://hrc-de-data/processed/cycling-extras/stations/')
+# ### Stations data
+# We are going to update the stations data (previously saved by another process) with some additional stations that are not present in the original stations data but are seen in some journey.
 
-# create temporary table for both weather and journey
+# read previously saved stations data from parquet
+df_processed_stations= spark.read.parquet('s3a://hrc-de-data/processed/cycling-dimension/stations/')
+
+df_processed_stations.tail(2)
+
+# create temporary table for both stations and journey
 df_journey.createOrReplaceTempView('journey')
-df_processed_stations.createOrReplaceTempView('stations')
+df_processed_stations.createOrReplaceTempView('station')
 
 
+# we keep all the stations which are not found in the temp view station table
 additional_stations= spark.sql('''
 select distinct(start_station) as station_id, `StartStation Name` as station_name 
 from journey 
-where start_station not in (select station_id from stations)
+where start_station not in (select station_id from station)
 union
 select distinct(end_station) as station_id, `EndStation Name` as station_name 
 from journey 
-where end_station not in (select station_id from stations)
+where end_station not in (select station_id from station)
 ''')
 additional_stations.show()
 
+
 # add columns to the additional stations to avoid errors when merging it to the previous one (df_processed_stations)
-additional_stations= additional_stations.withColumn('longitude', F.lit(0)).withColumn('latitude', F.lit(0))\
-    .withColumn('easting', F.lit(0)).withColumn('northing', F.lit(0))
+additional_stations= additional_stations.withColumn('longitude', lit(0).cast(DoubleType())).withColumn('latitude', lit(0).cast(DoubleType())).withColumn('easting', lit(0).cast(DoubleType())).withColumn('northing', lit(0).cast(DoubleType()))
 
-additional_stations = additional_stations.withColumn('longitude', additional_stations.longitude.cast(T.DoubleType()))\
-    .withColumn('latitude', additional_stations.latitude.cast(T.DoubleType()))\
-    .withColumn('easting', additional_stations.easting.cast(T.DoubleType()))\
-    .withColumn('northing', additional_stations.northing.cast(T.DoubleType()))
+additional_stations.show(5)
+additional_stations.printSchema()
 
-# make sure that the additional stations does not contain duplicated entries
+
+# remove duplicate values
 additional_stations.dropDuplicates()
 
 
-# save stations data into parquet files in s3
-additional_stations.write.parquet('s3a://hrc-de-data/processed/cycling-extras/stations/', mode='append')
-
-# drop other unnecessary journey column
-df_journey= df_journey.drop('StartStation Name', 'EndStation Name')
+# save additional stations data into parquet files in s3
+additional_stations.write.parquet('s3a://hrc-de-data/processed/cycling-dimension/stations/', mode='append')
 
 
-# save journey data into parquet files in s3
-df_journey.write.parquet('s3a://hrc-de-data/processed/cycling-journey/', mode='append')
+# drop other unnecessary journey columns
+df_journey= df_journey.drop('StartStation Name', 'EndStation Name', 'Duration')
+
+
+# ### Datetime
+# We are going to create/update datetime data from the start and end date of each journey.
+
+# extract datetime values from the start and the end date
+df_datetime_from_start= (
+    df_journey.select(
+        col('start_date').alias('datetime_id'), 
+        year(col('start_date')).alias('year'), 
+        month(col('start_date')).alias('month'), 
+        dayofmonth(col('start_date')).alias('day'),
+        hour(col('start_date')).alias('hour'),
+        minute(col('start_date')).alias('minute'),
+        second(col('start_date')).alias('second'),
+    )
+)
+df_datetime_from_end= (
+    df_journey.select(
+        col('end_date').alias('datetime_id'), 
+        year(col('end_date')).alias('year'), 
+        month(col('end_date')).alias('month'), 
+        dayofmonth(col('end_date')).alias('day'),
+        hour(col('end_date')).alias('hour'),
+        minute(col('end_date')).alias('minute'),
+        second(col('end_date')).alias('second'),
+    )
+)
+
+df_datetime_from_start.show(3)
+df_datetime_from_end.show(3)
+
+
+# combine the dataframes
+df_datetime= df_datetime_from_start.union(df_datetime_from_end)
+
+# remove duplicate entries
+df_datetime.dropDuplicates()
+
+df_datetime.show(10)
+
+
+# save datetime data into parquet files in s3
+df_datetime.write.parquet('s3a://hrc-de-data/processed/cycling-dimension/datetime/', mode='append')
+
+
+# finally, save journey data into parquet files in s3
+df_journey.write.parquet('s3a://hrc-de-data/processed/cycling-fact/journey/', mode='append')
--- airflow/dags/scripts/one-time-data-transformation.py
+++ airflow/dags/scripts/one-time-data-transformation.py
@@ -11,19 +11,22 @@ import os
 
 pyspark.__version__
 
-from pyspark.sql import SparkSession, functions as F, types as T
+from pyspark.sql import SparkSession
 
 spark = SparkSession.builder\
+   .master('local[*]')\
    .appName('data-transformer')\
-   .enableHiveSupport()\
    .getOrCreate()
 
-
 sc = spark.sparkContext
-# sc._jvm.org.apache.hadoop.util.VersionInfo.getVersion()
 
 df_stations = spark.read.csv("s3a://hrc-de-data/raw/cycling-extras/stations.csv", inferSchema=True, header=True)
+df_stations.take(2)
 
+df_stations.printSchema()
+
+
+from pyspark.sql import functions as F, types as T
 
 # rename columns
 stations= df_stations.withColumnRenamed('Station.Id', 'station_id')\
@@ -31,37 +34,54 @@ stations= df_stations.withColumnRenamed('Station.Id', 'station_id')\
    .withColumnRenamed('easting', 'easting')\
    .withColumnRenamed('northing', 'northing') 
 
-# count missing values in each column
-stations.select([F.count(F.when(F.isnan(c) | F.col(c).isNull(), c)).alias(c) for c in stations.columns]
-   ).show()
+stations.show(5)
 
-stations.write.parquet('s3a://hrc-de-data/processed/cycling-extras/stations/', mode='overwrite')
+
+# count missing values in each column
+stations.select([F.count(F.when(F.isnan(c) | F.col(c).isNull(), c)).alias(c) for c in stations.columns]).show()
+
+stations.write.parquet('s3a://hrc-de-data/processed/cycling-dimension/stations/', mode='overwrite')
+
+
+# ### 2. Weather data
 
 df_weather = spark.read.json("s3a://hrc-de-data/raw/cycling-extras/weather.json")
 
+df_weather.take(2)
+
+df_weather.printSchema()
+
 # drop some columns that we won't need
 weather= df_weather.drop('cloudcover', 'conditions', 'datetimeEpoch', 'description', 'dew', 'icon', 
-                            'precipcover', 'source', 'stations', 'sunriseEpoch', 'sunsetEpoch')
+                           'precipcover', 'source', 'stations', 'sunriseEpoch', 'sunsetEpoch')
+
 
 # transform datetime
 weather= weather.withColumnRenamed('datetime', 'weather_date') 
 weather= weather.withColumn('weather_date', weather.weather_date.cast(T.DateType()))
 
+weather.printSchema()
+print(len(weather.columns), 'columns')
+
+
 # count missing values in each column
 cols= weather.columns
 cols.remove('weather_date')
+
 missing_values= weather.select([F.count(F.when(F.col(c).isNull() | F.isnan(c), c)).alias(c) for c in cols])
 
 missing_values.show()
 
+
 perc_missing_values= weather.select([(F.count(F.when(F.isnan(c) | F.col(c).isNull(), c))/F.count(F.lit(1))).alias(c) for c in cols])
 perc_missing_values.show()
+
 
 # drop columns where missing values are more than 70%
 
 weather= weather.drop('precipprob', 'preciptype', 'snow', 'snowdepth')
+weather.columns
 
 weather= weather.repartition(10)
 
-weather.write.parquet('s3a://hrc-de-data/processed/cycling-extras/weather/', mode='overwrite')
-
+weather.write.parquet('s3a://hrc-de-data/processed/cycling-dimension/weather/', mode='overwrite')
